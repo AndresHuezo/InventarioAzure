9d2a8b4f3a56662896ebc209971dd4698b73ca06
Victor <victormendez7@outlook.es>
2022-05-13 21:50:40 -0600

    Cambios nombre de columnas

--- InventarioAzure/Models/DB/InventarioContext.cs
+++ InventarioAzure/Models/DB/InventarioContext.cs
@@ -30,8 +30,8 @@ namespace InventarioAzure.Models.DB
             if (!optionsBuilder.IsConfigured)
             {
                 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                //optionsBuilder.UseSqlServer("Server=localhost;Database=Inventario; Trusted_Connection=True;");
-                optionsBuilder.UseSqlServer("Data Source=SQL8001.site4now.net;Initial Catalog=db_a858ce_inventario;User Id=db_a858ce_inventario_admin;Password=Andres09081996");
+                optionsBuilder.UseSqlServer("Server=localhost;Database=Inventario; Trusted_Connection=True;");
+                //optionsBuilder.UseSqlServer("Data Source=SQL8001.site4now.net;Initial Catalog=db_a858ce_inventario;User Id=db_a858ce_inventario_admin;Password=Andres09081996");
 
             }
         }
